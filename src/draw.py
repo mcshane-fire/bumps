eeb27c4dfa4e3b328b2053111ead90a6d11e45d6
Tim Granger <tim@mcshane.org>
2023-10-29 19:34:24 +0000

    Draw sandwich boats when their division doesn't race even if rowing over

--- src/draw.py
+++ src/draw.py
@@ -51,8 +51,8 @@ def draw_divisions(svg_config, out, xoff, yoff, event, space, draw_colours = Fal
                 if tmp < event['div_size'][day][d]:
                     if event['completed'][day][d] == False:
                         div_raced = False
-                    # cope with sandwich crews bumping up when their own division didn't race
-                    if div_raced == False and up is not None and up > 0:
+                    # cope with sandwich crews when their own division didn't race but the division above did race
+                    if div_raced == False and d > 0 and event['completed'][day][d-1] and up is not None and tmp == 0:
                         div_raced = True
                     break
                 tmp -= event['div_size'][day][d]
